e492b4e327a34e6bc16164c6498712bd93739f39
Gillian Oliveira <dev.gillian@gmail.com>
2024-11-10 14:17:26 -0300

    1004 simple product solution

---
+++ 1004-simple-product.js
@@ -0,0 +1,9 @@
+// 1004 - Produto Simples
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let A = parseInt(lines.shift());
+let B = parseInt(lines.shift());
+let PROD = A * B;
+console.log(`PROD = ${PROD}`);
