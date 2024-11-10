51cbb1c5bbdc7891d73b6916310fb75181641dbd
Gillian Oliveira <dev.gillian@gmail.com>
2024-11-10 14:15:40 -0300

    1001 extremely basic solution

---
+++ 1001-extremely-basic.js
@@ -0,0 +1,9 @@
+// 1001 - Extremamente Básico
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let A = parseInt(lines.shift());
+let B = parseInt(lines.shift());
+let X = A + B;
+console.log(`X = ${X}`);
